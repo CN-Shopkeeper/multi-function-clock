e530048181026e8aba39f80fcb867498e84a1f31
yuan <1447136316@qq.com>
2021-07-12 09:59:29 +0800

    y:修改不规范命名和菜单显示

--- fun_a_neg.asm
+++ fun_a_neg.asm
@@ -6,33 +6,33 @@ fun_a_neg proc
 	call init_val
 	mov ax,offset fun_a_neg_menu
 	call dispmsg
-fun_b_chooser:
+fun_a_neg_chooser:
 	call readc_my	;出口参数al=char ascii
 	;switch to corresponding function
-b_check_key_again:
+neg_check_key_again:
 	cmp al,'A'
-	jz b_start_timing
+	jz neg_start_timing
 	cmp al,'F'				;若要想退出时数码管不显示，可以调用clear子程序
 	jmp neg_timing_done
 	;wrong input dealing
 	mov ax,offset wrong_input_msg
 	call dispmsg
-	jmp fun_b_chooser
-b_start_timing:
+	jmp fun_a_neg_chooser
+neg_start_timing:
 	cmp count_flag,0
-	jz count_flag_set2
+	jz neg_count_flag_set2
 	mov count_flag,0
-	jmp b_start_timing_again
-	count_flag_set2:
+	jmp neg_start_timing_again
+	neg_count_flag_set2:
 	mov count_flag,2
-	b_start_timing_again:
+	neg_start_timing_again:
 	call sec_to_minsec
 	call show_time
 	cmp now_time,0
 	jz fun_a_neg_play_alarm
 	call readkey_my	;如果没有键盘输入则持续显示时间
-	jnz b_start_timing_again
-	jmp b_check_key_again	;有输入则检查功能
+	jnz neg_start_timing_again
+	jmp neg_check_key_again	;有输入则检查功能
 fun_a_neg_play_alarm:
 	call play_alarm
 neg_timing_done:
--- main.asm
+++ main.asm
@@ -25,11 +25,11 @@ include io.inc
 	;数字输入错误
 	readnum_input_error_msg byte 13,10,"please input a number between 0 and 9!",13,10,0
 	;功能选择器提示
-	fun_chooser_msg byte 13,10,30 dup('*'),13,10
+	fun_chooser_msg byte 13,10,35 dup('*'),13,10
 		byte "*    press A to set counter!    *",13,10
 		byte "*    press B to set alarm!      *",13,10
 		byte "*    press C to set music!      *",13,10
-		byte 13,10,30 dup('*'),13,10,0
+		byte 35 dup('*'),13,10,0
 	;音乐数据
 	include data_music.asm
 	;闹钟数据
@@ -62,6 +62,19 @@ start: mov ax,@data
 	out 21h,al
 	sti
 
+
+	;初始化时钟的小时
+	xor ax,ax
+    mov al,4
+    out 70h,al
+    in al,71h
+    mov bl,16
+    div bl
+	mov bh,ah
+	mov bl,10
+	mul bl
+	add al,bh
+	mov clock_hour,al
 	;初始化时钟的分钟
 	xor ax,ax
     mov al,2
